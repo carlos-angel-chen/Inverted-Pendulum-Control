0cd147e0b37151ee04b6643af7f4ae450e5eabc3
Carlos <cchen@itba.edu.ar>
2023-04-02 16:44:05 -0300

    final version of ejer2

--- Ejer2_matlab.m
+++ Ejer2_matlab.m
@@ -5,9 +5,9 @@ clear all; close all; clc
 %% Constantes del modelo.
 m = 0.1818; 
 M = 0.6042;
-Mt = 10;
+Mt = m+M;
 l = 0.09; %cm 
-g = 10;
+g = 9.81;
 J = 0.45e-3;
 %JJt = J+m*l^2;
 Jt = 2.744e-3;
@@ -67,47 +67,54 @@ D = double(D);
 Gss = ss(A,B,C,D);
 eig(A)
 
-%% Funcion de transferencia del modelo linealizado
+% Funcion de transferencia del modelo linealizado
 T = zpk(Gss)
 stepinfo(T);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% C2_1 - Loop shaping: INTERNO con T2
 s = tf('s');
 T2 = 1/(40*10*2*pi);   %Arranco considerando un periodo de muestreo chico, parto de una situación mas ideal. 
 Pade = (1-s*T2/4)/(1+s*T2/4);
 C2_1 = 1;
-Pap2 = 0.60216*(s+7.76)/(s-7.76);
-Pmp2 = 1/((s+7.76)^2);
+Pap2 = 8.6614*(s+8.172)/(s-8.172);
+Pmp2 = 1/((s+8.172)^2);
 L2_1 = Pap2*Pmp2*C2_1*Pade
 figure('Name', 'Bode2_1'); bode(L2_1)
 margin(L2_1)
 grid on
 
-%% C2_2 - Accion integral y elimino polos estables
-C2_2 = db2mag(34.7)*(1/Pmp2)*(1/s);
+%% C2_2 - Proponemos un control PD con el objetivo de estabilizar la salida
+% Proponemos una K y un alpha que multiplica al control para obtener un pm 
+% igual a 60
+K2 = 1;
+alpha = 1;
+C2_2 = K2*(s+8.172)/(s+alpha*8.172);
 L2_2 = Pap2*Pmp2*Pade*C2_2
 figure('Name', 'Bode2_1'); bode(L2_2)
 margin(L2_2)
 grid on
+T2_2 = L2_2/(1+L2_2);
+figure('Name', 'Step2_2'); step(T2_2);
+grid on;
+CS2_2 = C2_2/(1+L2_2)
+figure('Name', 'StepCS2_3'); step(CS2_2)
 
-%% C2_3 - Agregamos pendiente en bajas y altas frecuencias
-C2_3 = db2mag(37.5)*(1/Pmp2)*(1/s)*(1/(s/(30*20)+1)^2)*((s+1)/(s+0.1));
+%% C2_3 - Configuro los valores de K y alpha para tener una salida estable 
+% y pm = 60
+K2 = db2mag(55);
+alpha = 15;
+C2_3 = K2*(s+8.172)/(s+alpha*8.172);
 L2_3 = Pap2*Pmp2*Pade*C2_3
 figure('Name', 'Bode2_1'); bode(L2_3)
-margin(L2_3)
+margin(L2_3);
 grid on
-
-Tf2_3 = L2_3/(1+L2_3)
-figure('Name', 'Step2_3'); step(Tf2_3)
+T2_3 = L2_3/(1+L2_3);
+figure('Name', 'Step2_2'); step(T2_3);
+grid on;
 CS2_3 = C2_3/(1+L2_3)
 figure('Name', 'StepCS2_3'); step(CS2_3)
 
-%%
-nyqlog(L2_3)
-
 %% Hallar la planta que ve C1 (Reurison)
-
 s  = tf('s');
 C2 = C2_3; %controlador C2 (interno)
 C2    = ss(C2);
@@ -116,7 +123,7 @@ C2.y  = 'u2';
 Gss   = ss(A,B,C,0); %Sistema completo
 Gss.u = 'u'; %soma de u(C1) + u(C2)
 Gss.y = 'y';
-Sum = sumblk('u = u1 + u2');
+Sum = sumblk('u = u1 - u2');
 
 Sysq   = ss([],[],[],[1 0]); %q
 Sysq.u = 'y';
@@ -133,44 +140,94 @@ GC1 = zpk(GC1);
 %% C1_1 - Analizando la planta que ve el C1
 C1_1 = 1;
 P1 = GC1;
-pap = ((s-7.722)*(s+52.37))/((s+7.722)*(s-52.37));
-pmp = (0.10099*s*(s+600)^2*(s+7.722)*(s+0.1)*(s+7.722))/(s^2*(s+7.76)*(s+5.752)*(s+1.027)*(s^2 + 1238*s + 4.076e05)*(s+52.37));
+pap = (7.648-s)/(s+7.648);
+pmp = -(1.4526*(s+122.6)*(s+7.648)^2)/(s^2*(s+8.172)*(s^2+114.4*s+3869));
 
 L1_1 = C1_1*pap*pmp*Pade;
 figure('Name', 'Bode1_1'); bode(L1_1)
 margin(L1_1)
 grid on
 
+T1_1 = L1_1/(1+L1_1);
+figure('Name', "Step"), step(T1_1);
+grid on;
+
 %% C1_2 - Agrego accion integral y elimino polos estables
-%K1 = db2mag(60);
-K1 = db2mag(8);
+% Controlador impropio
+K1 = db2mag(6.1);
 C1_2 = (1/s)*(1/pmp);
+
 L1_2 = K1*C1_2*pap*pmp*Pade;
 figure('Name', 'Bode1_2'); bode(L1_2)
 margin(L1_2)
 grid on
 
-Tf1_2 = L1_2/(1+L1_2);
-figure('Name', "Step"), step(Tf1_2);
+T1_2 = L1_2/(1+L1_2);
+figure('Name', "Step"), step(T1_2);
 grid on;
 
-figure();
-nyqlog(L1_2)
+%% C1_3 - Agrego pendientes en altas y bajas frecuencias
+K1 = db2mag(53.9);
+C1_3 = (1/s)*(1/pmp)*(1/(s+20)^2)*((s+0.1)/(s));
 
-%% C1_3 - Agrego pendientes
-K1 = 1;
-C1_3 = (1/s)*(1/pmp)*(1/(s+10)^4);
 L1_3 = K1*C1_3*pap*pmp*Pade;
 figure('Name', 'Bode1_3'); bode(L1_3)
 margin(L1_3)
 grid on
 
-Tf1_3 = L1_3/(1+L1_3);
-figure('Name', "Step"), step(Tf1_3);
+T1_3 = L1_3/(1+L1_3);
+figure('Name', "Step"), step(T1_3);
 grid on;
 
-figure();
-nyqlog(L1_3)
+CS1_3 = C1_3/(1+L1_3);
+figure('Name', 'StepCS2_3'); step(CS1_3);
+grid on;
+
+% Error en estado estacionario
+1 - dcgain(T1_3)
+
+%% Graficos
+%Lazo Abierto
+L = L1_3;
+subplot(3,2,1)
+bode(L)
+title("Bode de L(s) lazo abierto")
+margin(L)
+grid on
+
+%Lazo Cerrado
+S  = 1/(1+L); 
+T  = L/(1+L);
+CS = C1_3/(1+L);
+PS = P1/(1+L);
+
+%Graficos bode
+subplot(3,2,2)
+bode(S,T,CS,PS)
+title("Bode de S(s),T(s),CS(s) y PS(s)")
+grid on
+
+%Accion de control
+subplot(3,2,3)
+step(CS)
+title("Accion de Control C(s)S(s)")
+grid on
+AccionDeControl_info = stepinfo(CS)
+
+%Respuesta temporal
+subplot(3,2,4)
+step(PS)
+title("Respuesta Temporal P(s)S(s)")
+grid on
+RespuestaPS_info = stepinfo(T)
+
+%Respuesta temporal
+subplot(3,2,5)
+step(T)
+title("Respuesta Temporal T(s)")
+grid on
+RespuestaTemporal_info = stepinfo(T)
+
 
 
 
--- test-Ejer2/test_ejer2.m
+++ test-Ejer2/test_ejer2.m
@@ -5,9 +5,9 @@ clear all; close all; clc
 %% Constantes del modelo.
 m = 0.1818; 
 M = 0.6042;
-Mt = 10;
+Mt = m+M;
 l = 0.09; %cm 
-g = 10;
+g = 9.81;
 J = 0.45e-3;
 %JJt = J+m*l^2;
 Jt = 2.744e-3;
@@ -67,29 +67,166 @@ D = double(D);
 Gss = ss(A,B,C,D);
 eig(A)
 
-%% Funcion de transferencia del modelo linealizado
-T = zpk(Gss);
+% Funcion de transferencia del modelo linealizado
+T = zpk(Gss)
 stepinfo(T);
 
-%% Planta del angulo
+%% C2_1 - Loop shaping: INTERNO con T2
 s = tf('s');
-P_ang = 0.60216/((s-7.76)*(s+7.76));
-Ts = 1/(5*10*2*pi);
-pade = (1 - s*Ts/4)/(1 + s*Ts/4);
-pap = pade*(s+7.76)/(s-7.76);
-pmp = 1/((s+7.76)^2);
-P = pap*pmp;
+T2 = 1/(40*10*2*pi);   %Arranco considerando un periodo de muestreo chico, parto de una situación mas ideal. 
+Pade = (1-s*T2/4)/(1+s*T2/4);
+C2_1 = 1;
+Pap2 = 8.6614*(s+8.172)/(s-8.172);
+Pmp2 = 1/((s+8.172)^2);
+L2_1 = Pap2*Pmp2*C2_1*Pade
+figure('Name', 'Bode2_1'); bode(L2_1)
+margin(L2_1)
+grid on
 
+%% C2_2 - Proponemos un control PD con el objetivo de estabilizar la salida
+% Proponemos una K y un alpha que multiplica al control para obtener un pm 
+% igual a 60
+K2 = 1;
+alpha = 1;
+C2_2 = K2*(s+8.172)/(s+alpha*8.172);
+L2_2 = Pap2*Pmp2*Pade*C2_2
+figure('Name', 'Bode2_1'); bode(L2_2)
+margin(L2_2)
+grid on
+T2_2 = L2_2/(1+L2_2);
+figure('Name', 'Step2_2'); step(T2_2);
+grid on;
+CS2_2 = C2_2/(1+L2_2)
+figure('Name', 'StepCS2_3'); step(CS2_2)
 
+%% C2_3 - Configuro los valores de K y alpha para tener una salida estable 
+% y pm = 60
+K2 = db2mag(55);
+alpha = 15;
+C2_3 = K2*(s+8.172)/(s+alpha*8.172);
+L2_3 = Pap2*Pmp2*Pade*C2_3
+figure('Name', 'Bode2_1'); bode(L2_3)
+margin(L2_3);
+grid on
+T2_3 = L2_3/(1+L2_3);
+figure('Name', 'Step2_2'); step(T2_3);
+grid on;
+CS2_3 = C2_3/(1+L2_3)
+figure('Name', 'StepCS2_3'); step(CS2_3)
 
+%% Hallar la planta que ve C1 (Reurison)
+s  = tf('s');
+C2 = C2_3; %controlador C2 (interno)
+C2    = ss(C2);
+C2.u  = 'tita';
+C2.y  = 'u2';
+Gss   = ss(A,B,C,0); %Sistema completo
+Gss.u = 'u'; %soma de u(C1) + u(C2)
+Gss.y = 'y';
+Sum = sumblk('u = u1 - u2');
 
+Sysq   = ss([],[],[],[1 0]); %q
+Sysq.u = 'y';
+Sysq.y = 'q';
 
+Systita   = ss([],[],[],[0 1]); %tita
+Systita.u = 'y';
+Systita.y = 'tita';
 
+% Transferencia "vista" por C1.
+GC1 = connect(Gss, C2, Sum, Sysq, Systita, 'u1', 'q');
+GC1 = zpk(GC1);
 
+%% C1_1 - Analizando la planta que ve el C1
+C1_1 = 1;
+P1 = GC1;
+pap = (7.648-s)/(s+7.648);
+pmp = -(1.4526*(s+122.6)*(s+7.648)^2)/(s^2*(s+8.172)*(s^2+114.4*s+3869));
 
+L1_1 = C1_1*pap*pmp*Pade;
+figure('Name', 'Bode1_1'); bode(L1_1)
+margin(L1_1)
+grid on
 
+T1_1 = L1_1/(1+L1_1);
+figure('Name', "Step"), step(T1_1);
+grid on;
 
+%% C1_2 - Agrego accion integral y elimino polos estables
+% Controlador impropio
+K1 = db2mag(6.1);
+C1_2 = (1/s)*(1/pmp);
 
+L1_2 = K1*C1_2*pap*pmp*Pade;
+figure('Name', 'Bode1_2'); bode(L1_2)
+margin(L1_2)
+grid on
+
+T1_2 = L1_2/(1+L1_2);
+figure('Name', "Step"), step(T1_2);
+grid on;
+
+%% C1_3 - Agrego pendientes en altas y bajas frecuencias
+K1 = db2mag(53.9);
+C1_3 = (1/s)*(1/pmp)*(1/(s+20)^2)*((s+0.1)/(s));
+
+L1_3 = K1*C1_3*pap*pmp*Pade;
+figure('Name', 'Bode1_3'); bode(L1_3)
+margin(L1_3)
+grid on
+
+T1_3 = L1_3/(1+L1_3);
+figure('Name', "Step"), step(T1_3);
+grid on;
+
+CS1_3 = C1_3/(1+L1_3);
+figure('Name', 'StepCS2_3'); step(CS1_3);
+grid on;
+
+% Error en estado estacionario
+1 - dcgain(T1_3)
+
+%% Graficos
+%Lazo Abierto
+L = L1_3;
+subplot(3,2,1)
+bode(L)
+title("Bode de L(s) lazo abierto")
+margin(L)
+grid on
+
+%Lazo Cerrado
+S  = 1/(1+L); 
+T  = L/(1+L);
+CS = C1_3/(1+L);
+PS = P1/(1+L);
+
+%Graficos bode
+subplot(3,2,2)
+bode(S,T,CS,PS)
+title("Bode de S(s),T(s),CS(s) y PS(s)")
+grid on
+
+%Accion de control
+subplot(3,2,3)
+step(CS)
+title("Accion de Control C(s)S(s)")
+grid on
+AccionDeControl_info = stepinfo(CS)
+
+%Respuesta temporal
+subplot(3,2,4)
+step(PS)
+title("Respuesta Temporal P(s)S(s)")
+grid on
+RespuestaPS_info = stepinfo(T)
+
+%Respuesta temporal
+subplot(3,2,5)
+step(T)
+title("Respuesta Temporal T(s)")
+grid on
+RespuestaTemporal_info = stepinfo(T)
 
 
 
